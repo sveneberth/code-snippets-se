8db1ce5dd4f6df1b72f38cf7be3e6ed050a249db
Sven Eberth <Sven.Eberth@gmail.com>
2014-08-18 15:00:28 +0200

    further made...

--- functions.js
+++ functions.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+	//--- check if presed enter ---
 	$.fn.pressEnter = function(fn) {
 		return this.each(function() {
 			$(this).bind('enterPress', fn);
@@ -9,14 +10,18 @@ $(document).ready(function() {
 			})
 		});  
 	};
+	
+	//---reload a image
 	$.fn.reloadimg = function(fn) {
 		return this.each(function(index) {
 			var src = $(this).attr('src');
 			$(this).attr('src', src);
-			console.log("set by element img["+index+"] attribute src on '"+src+"'");
+			console.log("reload image:\nimg="+index+"\nsrc='"+src+"'");
 		});  
 	};
 });
+
+//--- function basename (like PHP) ---
 function basename(path) {
 	var b = path;
 	var lastChar = b.charAt(b.length - 1);
@@ -26,6 +31,25 @@ function basename(path) {
 	b = b.replace(/^.*[\/\\]/g, '');
 	return b;
 }
+
+//--- function empty (like PHP) ---
 function empty(str) {
     return (!str || 0 === str.length);
 }
+
+//--- emailpattern ---
+var emailpattern = new RegExp('^([a-zA-Z0-9\\-\\.\\_]+)(\\@)([a-zA-Z0-9\\-\\.]+)(\\.)([a-zA-Z]{2,4})$');
+
+//--- Scroll to place ---
+$(document).ready(function() {
+	$(function(){
+		$("a[href^='#']").click(function(event) {
+			event.preventDefault();
+			var target = $(this).attr('href');
+			$('html,body').animate({
+				scrollTop: $(target).offset().top-30
+			},1000);
+			console.log("Scroll to "+target);
+		});
+	});
+});
